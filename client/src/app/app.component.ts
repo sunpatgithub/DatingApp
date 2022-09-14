70096eb77974028b0d346fd2a87f87153e95aa69
Your Name <you@example.com>
2022-09-14 13:11:13 -0700

    End of Section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    console.log(1);
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      console.log(2);
+      console.log(response);
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+}
